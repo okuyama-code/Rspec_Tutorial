fb02928045a19b826eb2bfbf9c9c3ccf3644dd1d
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-03-05 18:53:42 +0900

    SQL のログ表示について spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,9 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
